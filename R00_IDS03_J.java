083e95ca8a19fc096aa541fe60fd9dfd6831e70c
ari557 <ar165909@ucf.edu>
2026-03-07 12:47:19 -0500

    R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,33 +1,15 @@
-import java.util.logging.Logger;
-import java.util.regex.Pattern;
+/*
+Rule 00. Input Validation and Data Sanitization (IDS)
+IDS03-J
+*/
 
-public class R00_IDS03_J {
+if (loginSuccessful){
+    logger.severe("User login succeeded for:" +sanitizeUser(username));
+}else{
+    logger.severe("User login failed for: " +sanitizeUser(username));
+}
 
-    static Logger logger = Logger.getLogger("LoginLogger");
-
-    public static void main(String[] args) {
-        String username = "user123";
-        boolean loginSuccessful = true;
-
-        // Non-compliant code
-        if (loginSuccessful) {
-            logger.severe("User login succeeded for: " + username);
-        } else {
-            logger.severe("User login failed for: " + username);
-        }
-
-        // Compliant code
-        if (loginSuccessful) {
-            logger.severe("User login succeeded for: " + sanitizeUser(username));
-        } else {
-            logger.severe("User login failed for: " + sanitizeUser(username));
-        }
-    }
-
-    public static String sanitizeUser(String username) {
-        if (Pattern.matches("[A-Za-z0-9]+", username)) {
-            return username;
-        }
-        return "unauthorized user";
-    }
+public String sanitizeUser(String username){
+    return Pattern.matches("[A-Za-z0-9_]+", username))
+    ? username+ "unauthorized user";
 }
