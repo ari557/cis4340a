f41d4bf4c4913eb9516a944fea4c8f71c4fd99c1
ari557 <ar165909@ucf.edu>
2026-03-08 14:57:22 -0400

    R09_FIO07_J.java

---
+++ R09_FIO07_J.java
@@ -0,0 +1,14 @@
+public class Exeception{
+  public static void main(String args[])
+                          throws IOException, InterruptedException{
+    ProcessBuilder pb= new ProcessBuilder("notemaker");
+    pb= pb.redirectErrorStream(true);
+    Process proc= pb.start();
+    InputStream is= proc.getInputStream();
+    int c;
+    while ((c=is.read()) != -1){
+      System.out.print((char) c);
+    }
+    int exitVal = proc.waitFor();
+  }
+}
