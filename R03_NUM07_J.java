60cf8c89f71772a448f2ef4757e699aeefae9685
ari557 <ar165909@ucf.edu>
2026-03-08 14:50:03 -0400

    R07_THI_01.java

--- R03_NUM07_J.java
+++
@@ -1,9 +0,0 @@
-public class NaNComparison {
-  public static void main(String[] args){
-    double x=0.0;
-    double result = Math.cos(1/x);
-    if(Double.isNaN(result)){
-      System.out.println("result is NaN";
-    }
-  }
-}
